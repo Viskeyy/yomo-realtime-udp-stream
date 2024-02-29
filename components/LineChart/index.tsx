71643a33e39dd3c8e06f735231f28f41c633dcd6
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2024-02-29 18:56:37 +0800

    fix: window undefined

--- components/LineChart/index.tsx
+++ components/LineChart/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 import { Line, LineChart, Tooltip, YAxis } from 'recharts';
 
-const screenWidth = window?.innerWidth || 1200;
+// const screenWidth = typeof window === undefined ? 1200 : window.innerWidth;
 
 export default function CustomLineChart(data: { data: any[] | undefined }) {
     return (
         <div>
-            <LineChart width={screenWidth - 20} height={300} data={data.data} margin={{ top: 10, right: 0, bottom: 10, left: 0 }}>
+            <LineChart width={1200} height={300} data={data.data} margin={{ top: 10, right: 0, bottom: 10, left: 0 }}>
                 <Line type='monotone' dataKey='UdpInDatagrams' stroke='#0761d1' dot={false} activeDot={false} animationDuration={500} strokeWidth={2} />
                 <Line type='monotone' dataKey='UdpOutDatagrams' stroke='#7928ca' dot={false} activeDot={false} animationDuration={500} strokeWidth={2} />
                 <YAxis />
