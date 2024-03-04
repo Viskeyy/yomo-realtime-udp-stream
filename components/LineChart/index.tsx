e4eab4a85bf47141cf99a3cb9e5273553480630c
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2024-03-04 17:24:47 +0800

    fix: line chart width fit window width

--- components/LineChart/index.tsx
+++ components/LineChart/index.tsx
@@ -1,12 +1,16 @@
 'use client';
 import { Line, LineChart, Tooltip, YAxis } from 'recharts';
 
-// const screenWidth = typeof window === undefined ? 1200 : window.innerWidth;
-
 export default function CustomLineChart(data: { data: any[] | undefined }) {
+    let screenWidth = 1500;
+
+    if (typeof window !== 'undefined') {
+        screenWidth = window.innerWidth;
+    }
+
     return (
         <div>
-            <LineChart width={1200} height={300} data={data.data} margin={{ top: 10, right: 0, bottom: 10, left: 0 }}>
+            <LineChart width={screenWidth - 20} height={300} data={data.data} margin={{ top: 10, right: 0, bottom: 10, left: 0 }}>
                 <Line type='monotone' dataKey='UdpInDatagrams' stroke='#0761d1' dot={false} activeDot={false} animationDuration={500} strokeWidth={2} />
                 <Line type='monotone' dataKey='UdpOutDatagrams' stroke='#7928ca' dot={false} activeDot={false} animationDuration={500} strokeWidth={2} />
                 <YAxis />
